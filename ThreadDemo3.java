f4d2f7e874997ca688b8b36616177f66963a2bdb
draj888 <31967338+draj888@users.noreply.github.com>
2018-09-11 15:47:53 +0530

    Update ThreadDemo3.java

--- ThreadDemo3.java
+++ ThreadDemo3.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author rajendra
@@ -28,7 +22,7 @@ public class ThreadDemo3
         
         try
         {
-            t.join();
+            t.join();//wait for thread t to finish.
         }
         catch(InterruptedException e)
         {
