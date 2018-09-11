728f1959b485573d4238ff183b96904056966b34
draj888 <31967338+draj888@users.noreply.github.com>
2018-09-11 12:38:41 +0530

    Update ThreadDemo2.java

--- ThreadDemo2.java
+++ ThreadDemo2.java
@@ -13,12 +13,24 @@ class MyRunnable implements Runnable //insted of extending Thread class we can i
     }
 }
 
+class MyThread2 extends Thread //insted of extending Thread class we can implement Runnable interface. Much beneficial while inheriting from other classes.
+{
+    public void run()//mandatory to override.
+    {
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("i = "+i+"\n");
+        }
+    }
+}
+
 public class ThreadDemo2 
 {
     public static void main(String args[])
     {
         MyRunnable r = new MyRunnable();//object of MyRunnable
-        Thread t = new Thread(r); //start can't be called on runnable object. Pass runnable object as parameter to Thread object.
+        MyThread2 t2 = new MyThread2();
+        Thread t = new Thread(t2); //start can't be called on runnable object. Pass runnable object as parameter to Thread object.
         t.start();//Call start on thread object. Directly calling run() doesn't creates a new thread.
         for(int j=0;j<10;j++)
         {
